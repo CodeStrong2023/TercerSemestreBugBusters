f174a66d7426fb0758c159952c491dd9c46a1b66
Franco Pizarro <pizarrofranco25@gmail.com>
2024-04-15 20:33:27 -0300

    Ciclo Do-While JS

--- JavaScript/clase1/01-03-ciclos.js
+++ JavaScript/clase1/01-03-ciclos.js
@@ -1,7 +1,15 @@
-//Ciclo while
+//while
 let contador = 0;
-while (contador < 3) {
-    console.log(contadopr);
+while(contador < 3){
+    console.log(contador);
     contador++;
 }
 console.log("Fin del ciclo while");
+
+//do while
+let conteo = 0;
+do{
+    console.log(conteo);
+    conteo++;
+}while(conteo < 3);
+console.log("Fin del ciclo do while");
