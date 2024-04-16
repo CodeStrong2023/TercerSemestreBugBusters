e9acd1ba43e3c14dd51e5ddcc3b5a068e3abd00b
David Abadie <abadiedaviiid@gmail.com>
2024-04-15 21:42:03 -0300

    video 9 clase 1 python

---
+++ Python/clase1/Rectangulo.py
@@ -0,0 +1,13 @@
+from Color import Color
+from FiguraGeometrica import FiguraGeometrica
+
+class Rectangulo(FiguraGeometrica, Color):
+    def __init__(self, ancho, alto, color):
+        FiguraGeometrica.__init__(self, ancho, alto)
+        Color.__init__(self, color)
+
+    def calcular_area(self):
+        return self.alto * self.ancho
+
+    def __str__(self):
+        return f'{FiguraGeometrica.__str__(self)} {Color.__str__()}'
