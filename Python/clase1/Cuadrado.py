d174794c7ce3c0e86a2a608d6c2eff10a34dd7fc
Nicolas Nuñez <nuneznicolas582@gmail.com>
2024-04-15 21:29:43 -0300

    Video 8 Clase 1 Python

--- Python/clase1/Cuadrado.py
+++ Python/clase1/Cuadrado.py
@@ -9,3 +9,6 @@ class Cuadrado (FiguraGeometrica, Color):
         
     def Calcular_area(self):
         return self.alto * self.ancho
+
+    def __str__(self):
+        return f"{FiguraGeometrica.__str__(self)} {Color.__str__(self)}"
