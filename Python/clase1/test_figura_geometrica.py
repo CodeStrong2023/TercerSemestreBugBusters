bcddcfe5991c1b4422240e9da9003515348dbf2c
Santiago Segura <santiagosegura15@hotmail.com>
2024-04-15 21:11:58 -0300

    Subo codigo metodo MRO clase 1 py

--- Python/clase1/test_figura_geometrica.py
+++ Python/clase1/test_figura_geometrica.py
@@ -4,3 +4,12 @@ cuadrado1= Cuadrado(5, 'Azul')
 print(cuadrado1.ancho)
 print(cuadrado1.alto)
 print(f'Cálculo del área del cuadrado : {cuadrado1.Calcular_area()}')
+
+#MRO = Method Resolution Order
+print(Cuadrado.mro())
+
+print(cuadrado1)
+
+rectangulo1 = Rectangulo(2, 8, 'verde')
+print(f'Calculo del area del rectangulo: {rectangulo1.calcular_area()}')
+print(rectangulo1)
