e9b233d7fdf6adee14d87100369be72bda0c3fb1
Nicolas Nuñez <nuneznicolas582@gmail.com>
2024-04-19 11:28:16 -0300

    Arreglo en codigo de la clase 1 de Python

--- Python/clase1/test_figura_geometrica.py
+++ Python/clase1/test_figura_geometrica.py
@@ -1,4 +1,5 @@
 from Cuadrado import Cuadrado
+from Rectangulo import Rectangulo
 
 cuadrado1= Cuadrado(5, 'Azul')
 print(cuadrado1.ancho)
@@ -10,6 +11,6 @@ print(Cuadrado.mro())
 
 print(cuadrado1)
 
-rectangulo1 = Rectangulo(2, 8, 'verde')
+rectangulo1 = Rectangulo(2,8, "verde")
 print(f'Calculo del area del rectangulo: {rectangulo1.calcular_area()}')
 print(rectangulo1)
