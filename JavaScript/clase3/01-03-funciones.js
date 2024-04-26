8d79e70a40da54c3a638bd804dc19bb977fd347a
David Abadie <abadiedaviiid@gmail.com>
2024-04-26 17:56:17 -0300

    video 7 clase 3

--- JavaScript/clase3/01-03-funciones.js
+++ JavaScript/clase3/01-03-funciones.js
@@ -40,3 +40,12 @@ console.log(miFuncionTexto);
 const sumarFuncionTipoFlecha = (a, b) => a + b;
 resultado = sumarFuncionTipoFlecha(3, 2);
 console.log(resultado);
+
+//Funcion tipo expresion
+let sumar = function(a = 4, b = 8){
+    console.log(arguments[0]); //muestra el parametro de: a
+    console.log(arguments[1]); //muestra el parametro de: b
+    return a + b + arguments[2];
+}
+resultado = sumar(3, 2, 9);
+console.log(resultado);
