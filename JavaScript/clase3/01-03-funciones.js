98291c70d3b362131c05b1cc61a101a194762e0a
Franco Pizarro <pizarrofranco25@gmail.com>
2024-04-26 15:47:44 -0300

    Javascript clase 3 video 3 y 4

--- JavaScript/clase3/01-03-funciones.js
+++ JavaScript/clase3/01-03-funciones.js
@@ -1,13 +1,24 @@
-miFuncion(8, 2); //Esto se conoce como hosting
+miFuncion(8,2); //Esto se le conoce como hosting
 
-function miFuncion(a, b,){
-    //console.log("Sumamos: "+ (a+b));
+function miFuncion(a, b){
+    //console.log("Sumamos: "+ (a+ b ));
     return a + b;
 }
 
-//Lllamamos la funcion 
+//Llamando la función
 miFuncion(5, 4);
 
 let resultado = miFuncion(6, 7);
 console.log(resultado);
 
+//Declaramos una función de tipo expresión o anonima
+let x = function(a, b){
+    return a + b; //necesita cierre con punto y coma
+}
+resultado = x(5, 6); //al llamarla se pone la variable y parentesis
+console.log(resultado);
+
+//funciones de tipo self e invoking
+(function(a, b){
+    console.log('Ejecutando la funcion: ' + (a + b));
+})(9, 6);
