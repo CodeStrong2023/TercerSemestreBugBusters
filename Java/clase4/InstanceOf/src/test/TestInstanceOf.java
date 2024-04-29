321fdb8e363585b26597ee521c6bdb186afcbe8a
David Abadie <abadiedaviiid@gmail.com>
2024-04-29 18:46:13 -0300

    video 10 clase 4

--- Java/clase4/InstanceOf/src/test/TestInstanceOf.java
+++ Java/clase4/InstanceOf/src/test/TestInstanceOf.java
@@ -23,11 +23,14 @@ public class TestInstanceOf {
     public static void determinarTipo(Empleado empleado){
         if (empleado instanceof Gerente) {
             System.out.println("Es de tipo Gerente");
-            Gerente  gerente = (Gerente)empleado;
-            gerente.getDepartameto();
+            //Gerente  gerente = (Gerente)empleado;
+            ((Gerente) empleado).getDepartameto();
+            System.out.println("gerente = " + empleado);
         }
         else if (empleado instanceof Empleado) {
             System.out.println("Es de tipo Empleado");
+            //Gerente  gerente = (Gerente)empleado;
+            //System.out.println("gerente = "+ gerente.getDepartamento());
         }
         else if (empleado instanceof Object) {
             System.out.println("Es de tipo Object");
