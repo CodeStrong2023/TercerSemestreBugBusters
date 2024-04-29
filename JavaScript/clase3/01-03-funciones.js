c9f342018682c8d7d126ad0fae32ab33b25b6bde
Marcos Antonio Senn <marcossenn@gmail.com>
2024-04-29 20:23:34 -0300

    JS clase 3 Parte 10

--- JavaScript/clase3/01-03-funciones.js
+++ JavaScript/clase3/01-03-funciones.js
@@ -1,6 +1,6 @@
-miFuncion(8,2); //Esto se le conoce como hoisting
+miFuncion(8, 2); //Esto se le conoce como hoisting
 
-function miFuncion(a, b){
+function miFuncion(a, b) {
     //console.log("Sumamos: "+ (a+ b ));
     return a + b;
 }
@@ -12,25 +12,24 @@ let resultado = miFuncion(6, 7);
 console.log(resultado);
 
 //Declaramos una función de tipo expresión o anonima
-let x = function(a, b){
+let x = function (a, b) {
     return a + b; //necesita cierre con punto y coma
-}
+};
 resultado = x(5, 6); //al llamarla se pone la variable y parentesis
 console.log(resultado);
 
 //funciones de tipo self e invoking
-(function(a, b){
-    console.log('Ejecutando la funcion: ' + (a + b));
+(function (a, b) {
+    console.log("Ejecutando la funcion: " + (a + b));
 })(9, 6);
-
 
 //Las funciones son objetos
 console.log(typeof miFuncion);
-function miFuncionDos(a, b){
+function miFuncionDos(a, b) {
     console.log(arguments.length);
 }
 
-miFuncionDos(5, 7, 3, 6)
+miFuncionDos(5, 7, 3, 6);
 
 // toString
 var miFuncionTexto = miFuncionDos.toString();
@@ -42,20 +41,20 @@ resultado = sumarFuncionTipoFlecha(3, 2);
 console.log(resultado);
 
 //Funcion tipo expresion
-let sumar = function(a = 4, b = 8){
+let sumar = function (a = 4, b = 8) {
     console.log(arguments[0]); //muestra el parametro de: a
     console.log(arguments[1]); //muestra el parametro de: b
     return a + b + arguments[2];
-}
+};
 resultado = sumar(3, 2, 9);
 console.log(resultado);
 
 // sumar todos los argumentos
 let respuesta = sumarTodo(5, 4, 13, 10, 9);
 console.log(respuesta);
-function sumarTodo(){
+function sumarTodo() {
     let suma = 0;
-    for(let i = 0; i < arguments.length; i++){
+    for (let i = 0; i < arguments.length; i++) {
         suma += arguments[i]; //arguments es para arreglos
     }
     return suma;
@@ -63,9 +62,24 @@ function sumarTodo(){
 
 //Tipos primitivos
 let k = 10;
-function cambiarValor(a){//Paso por valor
-    a= 20;
+function cambiarValor(a) {
+    //Paso por valor
+    a = 20;
 }
 
 cambiarValor(k);
-console.log(k); 10
+console.log(k);
+10;
+
+const persona = {
+    nopmbre: "Juan",
+    apellido: "Perez",
+};
+console.log(persona);
+function cambiarValorObjeto(p1) {
+    p1.nombre = "Carlos";
+    p1.apellido = "Lara";
+}
+
+cambiarValorObjeto(persona);
+console.log(persona);
