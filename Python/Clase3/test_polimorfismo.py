459fe1c160567f284f835be8da57b51a7a0f7e14
Santiago Segura <santiagosegura15@hotmail.com>
2024-04-30 12:48:22 -0300

    Subo los cambios de la ultima parte de la clase 3 de py

--- Python/Clase3/test_polimorfismo.py
+++ Python/Clase3/test_polimorfismo.py
@@ -5,6 +5,8 @@ def imprimir_detalles(objeto):
     # print(objeto) # de manera indirecta llama al str de la clase Empleado o Gerente
     print(type(objeto)) # Esto es para saber el tiop de dato que recibe
     print(objeto.mostrar_detalles())
+    if (isinstance(objeto, Gerente)):
+        print(objeto.departamento)
 
 empleado = Empleado('Ariel', 50000.00)
 imprimir_detalles(empleado)
