412e113575427799c036adee00427dc7562b8331
Santiago Segura <santiagosegura15@hotmail.com>
2024-05-08 22:39:20 -0300

    Subo mi parte de js (video 3,4) y py (video 4) clase 5

--- Python/clase5/manejo_excepciones.py
+++ Python/clase5/manejo_excepciones.py
@@ -1,10 +1,17 @@
 resultado = None
-a = '10'
-b = 0
+a = 7
+b = 5
 try:
     resultado = a / b  # modificamos
+
+except TypeError as e:
+    print(f'TypeError - Ocurrio un error: {type(e)}')
+
 except ZeroDivisionError as e:
-    print(f'Ocurrió un error: {e}')
+    print(f'ZeroDivisionError - Ocurrio un error: {type(e)}')
+
+except  Exception as e:
+    print('Exception - Ocurrió un error: {type(e)}')
 
 print(f'El resultado es: {resultado}')
 print('seguimos...')
