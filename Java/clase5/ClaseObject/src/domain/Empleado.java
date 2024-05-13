01351264791efb42c6bc495f5c4119fc85fd999e
Nicolas Nuñez <nuneznicolas582@gmail.com>
2024-05-13 20:58:49 -0300

    Video 5 Clase 5 de Java

--- Java/clase5/ClaseObject/src/domain/Empleado.java
+++ Java/clase5/ClaseObject/src/domain/Empleado.java
@@ -4,6 +4,8 @@
  */
 package domain;
 
+import java.util.Objects;
+
 /**
  *
  * @author Franco
@@ -21,4 +23,54 @@ public class Empleado {
     public String obtenerDetalles(){
         return "Nombre: "+ this.nombre+", Sueldo: "+this.sueldo;
     }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "nombre=" + nombre + ", sueldo=" + sueldo + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.nombre);
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.sueldo) ^ (Double.doubleToLongBits(this.sueldo) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empleado other = (Empleado) obj;
+        if (Double.doubleToLongBits(this.sueldo) != Double.doubleToLongBits(other.sueldo)) {
+            return false;
+        }
+        return Objects.equals(this.nombre, other.nombre);
+    }
+    
+    
+    
 }
