97ee5974d84ce4d0e4296d86f2bafe38a798bba8
Franco Pizarro <pizarrofranco25@gmail.com>
2024-05-15 16:19:13 -0300

    Clase 6 video 5 y 6 en Python

--- Python/clase6/Archivos/leer_archivo.py
+++ Python/clase6/Archivos/leer_archivo.py
@@ -1,3 +1,7 @@
 
 archivo = open("prueba.txt", "r", encoding="utf8") # r es de read(leer)
-print(archivo.read())
+# print(archivo.read())
+print(archivo.read(16))
+print(archivo.read(10)) #Continuamos desde la línea anterior
+print(archivo.readline())
+print(archivo.readline())
--- Python/clase6/Archivos/manejo_archivos.py
+++ Python/clase6/Archivos/manejo_archivos.py
@@ -4,7 +4,9 @@ try:
     archivo.write('Programamos con diferentes tipos de archivos, ahora en txt \n')
     archivo.write("Los acentos son importantes para las palabras\n")
     archivo.write("como por ejemplo: acción, ejecución y producción\n")
-    archivo.write('Con esto terminamos')
+    archivo.write('Las letras son:\nr read = leer, \na append = agregar, \nw write = escribir, \nx crea un archivo')
+    archivo.write('\nt es para text = texto, \nb archivos binarios, \nw+ y r+ lee y escribe')
+    archivo.write('\nCon esto terminamos')
 except Exception as e:
     print(e)
 finally: #Siempre se ejecuta
