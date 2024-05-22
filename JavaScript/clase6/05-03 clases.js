e8f62ff8933409d22389a1ea2e95d448e3fe2a60
Marcos Antonio Senn <marcossenn@gmail.com>
2024-05-22 12:25:04 -0300

    JS clase 6 video 4

--- JavaScript/clase6/05-03 clases.js
+++ JavaScript/clase6/05-03 clases.js
@@ -1,5 +1,5 @@
-class Persona{
-    constructor({ nombre, apellido}) {
+class Persona {
+    constructor(nombre, apellido) {
         this._nombre = nombre;
         this._apellido = apellido;
     }
@@ -7,13 +7,18 @@ class Persona{
     get nombre() {
         return this._nombre;
     }
+
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
 }
 
-
-let persona1 = new Persona('Martin', 'Perez');
+let persona1 = new Persona("Martin", "Perez");
 //console.log(persona1);
 console.log(persona1.nombre);
-
-let persona2 = new Persona('Carlos', 'Lara');
+persona1.nombre = "Juan Carlos";
+console.log(persona1.nombre);
+let persona2 = new Persona("Carlos", "Lara");
+persona2.nombre = "Maria Laura";
 //console.log(persona2);
 console.log(persona2.nombre);
