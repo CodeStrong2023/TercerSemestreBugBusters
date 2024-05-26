76468996b6acf9b59d5495732cd7294728038c84
Marcos Antonio Senn <marcossenn@gmail.com>
2024-05-26 12:43:22 -0300

    py clase 6

--- Python/clase6/Archivos/leer_archivo.py
+++ Python/clase6/Archivos/leer_archivo.py
@@ -9,4 +9,12 @@ archivo = open("prueba.txt", "r", encoding="utf8") # r es de read(leer)
 #vamos a iterar el archivo,cada una de las lineas
 #for linea in archivo:
    # print(linea): iteramos todo los elementos del archivo
-print(archivo.readline()[11]): #accedemos al archivo como si fuera una lista
+#print(archivo.readline()[11]): #accedemos al archivo como si fuera una lista
+# anexamos infomacion, copiamos a otro
+
+archivo2 = open('copia.txt','2',encoding='utf8')
+archivo2.write(archivo.read())
+archivo.close() #cerramos primer archivo
+archivo2.close() #cerranos segundo archivo
+
+print('se ha terminado el proceso de leer y copiar archivos')
