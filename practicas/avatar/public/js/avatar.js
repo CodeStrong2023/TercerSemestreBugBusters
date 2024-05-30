ac8a324cfd9a8e9ecd21fc01a069b1fa9dabdb70
Santiago Segura <santiagosegura15@hotmail.com>
2024-05-29 22:55:33 -0300

    Subo la tarea de practicas de java del juego

--- practicas/avatar/public/js/avatar.js
+++ practicas/avatar/public/js/avatar.js
@@ -1,21 +1,40 @@
 function seleccionarPersonajeJugador() {
-  let personajeJugador = document.getElementsByName("personaje");
-
-  for (let personaje of personajeJugador) {
-    if(personaje.checked){
-        if (personaje.id == "zuko"){
-            alert("SELECCIONASTE A ZUKO");
-        }else if(personaje.id == "katara"){
-            alert("SELECCIONASTE A KATARA");
-        }else if(personaje.id == "aang"){
-            alert("SELECCIONASTE A AANG");
-        }else{
-            alert("SELECCIONASTE A TOPH");
+    let personajeJugador = document.getElementsByName("personaje");
+    let spanPersonajeJugador = document.getElementById("personaje-jugador");
+  
+    for (let personaje of personajeJugador) {
+      if (personaje.checked) {
+        if (personaje.id == "zuko") {
+          spanPersonajeJugador.innerHTML = "Zuko"
+        } else if (personaje.id == "katara") {
+          spanPersonajeJugador.innerHTML = "Katara"
+        } else if (personaje.id == "aang") {
+          spanPersonajeJugador.innerHTML = "Aang"
+        } else {
+          spanPersonajeJugador.innerHTML = "Toph"
         }
         break;
+      }
     }
   }
-}
+  
+  function seleccionarEnemigoRandom(){
+      let enemigoRandom = document.getElementById("personaje-enemigo");
+      let numRandom = Math.round(Math.random()*3+1);
+      if(numRandom == 1){
+          enemigoRandom.innerHTML = "Zuko"
+      }else if(numRandom == 2){
+          enemigoRandom.innerHTML = "Katara"
+      }else if(numRandom == 3){
+          enemigoRandom.innerHTML = "Aang"
+      }else{
+          enemigoRandom.innerHTML = "Toph"
+      }
+  }
+  
+  let botonPersonajeJugador = document.getElementById("boton-personaje");
+  botonPersonajeJugador.addEventListener("click", function(){
+      seleccionarPersonajeJugador(), seleccionarEnemigoRandom()});
 
-let botonPersonajeJugador = document.getElementById("boton-personaje");
-botonPersonajeJugador.addEventListener("click", seleccionarPersonajeJugador);
+
+window.addEventListener('load', iniciarJuego)
