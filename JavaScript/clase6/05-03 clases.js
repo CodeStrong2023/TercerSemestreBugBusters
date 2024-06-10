c89628e6d91b8c9a235193b90d0d9d11b7f1917e
FranquiPI <pizarrofranco25@gmail.com>
2024-06-10 15:41:15 -0300

    Clase 8 video 7 en Javascript

--- JavaScript/clase6/05-03 clases.js
+++ JavaScript/clase6/05-03 clases.js
@@ -6,10 +6,20 @@ class Persona { // Clase padre
     static contadorPersonas = 0; //Atributo estático
     //email = "Valor default email"; //Atributo no estático
 
+    static get MAX_OBJ(){ //Este método simula una constante
+        return 5;
+    }
+
     constructor(nombre, apellido) {
         this._nombre = nombre;
         this._apellido = apellido;
-        this.idPersona = ++Persona.contadorPersonas;
+        if(Persona.contadorPersonas < Persona.MAX_OBJ){
+            this.idPersona = ++Persona.contadorPersonas;
+        }
+        else{
+            console.log('Se ha superado el máximo de objetos permitidos')
+        }
+        
         //console.log("Se incrementa el contador: " + Persona.contadorObjetosPersona);
     }
 
@@ -115,3 +125,12 @@ console.log(Persona.contadorPersonas);
 let persona3 = new Persona('Carla', 'Pertosi');
 console.log(persona3.toString());
 console.log(Persona.contadorPersonas);
+
+console.log(Persona.MAX_OBJ);
+// Persona.MAX_OBJ = 10; //No se puede modificar, ni alterar
+console.log(Persona.MAX_OBJ);
+
+let persona4 = new Persona('Franco', 'Diaz');
+console.log(persona4.toString());
+let persona5 = new Persona('Liliana', 'Paz');
+console.log(persona5.toString());
