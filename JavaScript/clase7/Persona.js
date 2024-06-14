272ea3d3ecbcc594477ecfea4ba8d718c69ab00f
ChuequitoMendez <112666288+ChuequitoMendez@users.noreply.github.com>
2024-06-14 18:13:13 -0300

    Clase 9 js video 2 y 3

---
+++ JavaScript/clase7/Persona.js
@@ -0,0 +1,37 @@
+class Persona{
+
+    static contadorPersonas = 0;
+
+    constructor(nombre, apellido, edad){
+        this._idPersona = ++Persona.contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+
+    get idPersona(){
+        return this._idPersona;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+    get edad(){
+        return this._edad;
+    }
+
+    set edad(edad){
+        this._edad = edad;
+    }
+
+    toString(){
+        return `
+        ${this._idPersona} 
+        ${this._nombre} 
+        ${this._apellido} 
+        ${this._edad}`;
+    }
+    
+}
